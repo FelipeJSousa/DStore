d3db29bc21725ba62d25c4767ff720d56dc1a316
Felipe Sousa <csfelipe8@gmail.com>
2022-06-04 19:11:08 -0300

    Init Project

---
+++ src/Web/MVC/DS.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DS.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
